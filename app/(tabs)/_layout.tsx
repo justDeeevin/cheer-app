9385f9b93eda640f2241d35252d066be4d126092
Devin Droddy <devin.droddy@gmail.com>
2024-07-12 15:01:02 -0400

    auto-signin sideeffect in app-wide layout prior to instantiating the context provider

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,42 +1,11 @@
-import { firebaseContext } from '@/authContext';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
-import { useContext, useEffect } from 'react';
-import * as SecureStore from 'expo-secure-store';
-import { GoogleAuthProvider, OAuthProvider, signInWithCredential } from 'firebase/auth';
+import { useContext } from 'react';
 import { i18nContext } from '@/i18n';
 
 export default function TabLayout() {
-  const { auth } = useContext(firebaseContext);
-
   const i18n = useContext(i18nContext);
   const t = i18n.t.bind(i18n);
-
-  useEffect(() => {
-    const effect = async () => {
-      if (!auth.currentUser) {
-        const credentialType = await SecureStore.getItemAsync('authProvider');
-        if (credentialType === 'Google') {
-          const idToken = await SecureStore.getItemAsync('idToken');
-          const accessToken = await SecureStore.getItemAsync('accessToken');
-          const cred = GoogleAuthProvider.credential(idToken, accessToken);
-          await signInWithCredential(auth, cred);
-        }
-        else if (credentialType === 'Apple') {
-          const idToken = await SecureStore.getItemAsync('idToken') as string;
-          const accessToken = await SecureStore.getItemAsync('accessToken') as string;
-          const firebaseCred = new OAuthProvider('apple.com')
-            .credential({
-              idToken,
-              accessToken
-            });
-          await signInWithCredential(auth, firebaseCred);
-        }
-      }
-    }
-
-    effect()
-  }, [])
 
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,38 @@ import { Stack } from 'expo-router/stack';
 import { firebaseContext } from '@/authContext';
 import { auth, db } from '@/firebaseConfig';
 import { i18nContext, useI18n } from '@/i18n'
+import { useEffect } from 'react';
+import * as SecureStore from 'expo-secure-store';
+import { GoogleAuthProvider, OAuthProvider, signInWithCredential } from 'firebase/auth';
 
 export default function Layout() {
   const i18n = useI18n();
+
+  useEffect(() => {
+    const effect = async () => {
+      if (!auth.currentUser) {
+        const credentialType = await SecureStore.getItemAsync('authProvider');
+        if (credentialType === 'Google') {
+          const idToken = await SecureStore.getItemAsync('idToken');
+          const accessToken = await SecureStore.getItemAsync('accessToken');
+          const cred = GoogleAuthProvider.credential(idToken, accessToken);
+          await signInWithCredential(auth, cred);
+        }
+        else if (credentialType === 'Apple') {
+          const idToken = await SecureStore.getItemAsync('idToken') as string;
+          const accessToken = await SecureStore.getItemAsync('accessToken') as string;
+          const firebaseCred = new OAuthProvider('apple.com')
+            .credential({
+              idToken,
+              accessToken
+            });
+          await signInWithCredential(auth, firebaseCred);
+        }
+      }
+    }
+
+    effect()
+  }, [])
 
   return (
     <i18nContext.Provider value={i18n}>
