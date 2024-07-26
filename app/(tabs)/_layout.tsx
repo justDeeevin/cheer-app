ca2d8bfd01ce129c375c70b7aae4aff1e58b1aca
Devin Droddy <devin.droddy@gmail.com>
2024-07-26 14:35:31 -0400

    hide attendance tab if not logged in

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,10 +2,13 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 import { useContext } from 'react';
 import { i18nContext } from '@/i18n';
+import { loggedInContext } from '@/context';
 
 export default function TabLayout() {
   const i18n = useContext(i18nContext);
   const t = i18n.t.bind(i18n);
+
+  const loggedIn = useContext(loggedInContext);
 
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue', headerShown: false }}>
@@ -21,6 +24,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="attendance"
         options={{
+          // Here, `undefined` makes it use the tab name as the href
+          href: loggedIn ? undefined : null,
           title: t('attendance'),
           tabBarIcon: ({ color }) => (
             <FontAwesome size={28} name="calendar-check-o" color={color} />
